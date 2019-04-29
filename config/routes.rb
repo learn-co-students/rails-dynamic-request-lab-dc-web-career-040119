5983db60be3935fd57e840e8055ec92a683a6281
Matthew Lindsey <mbl@null.net>
2019-04-29 17:12:01 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  # explicit version
   # get "students", to: "students#index"
   # get "students/:id", to: "students#show"
+
+  #refactored version
   resources :students, only: [:index, :show]
 end
